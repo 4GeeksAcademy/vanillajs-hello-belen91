626e9e9ac0953fa540493abd32746a5749101516
Belén Romero <belenromeroborrero@gmail.com>
2025-05-12 16:14:03 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+
+  let whos = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let actions = ['ate', 'peed', 'crushed', 'broke'];
+  let whats = ['my homework', 'my phone', 'the car'];
+  let whens = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  function aleatorio(conjunto){
+    let aleatorizar = conjunto[Math.floor(Math.random() * conjunto.length)]; 
+    return aleatorizar; 
+  }
+
+  function generarExcusa(){
+    let who = aleatorio(whos); 
+    let action = aleatorio(actions);
+    let what = aleatorio(whats); 
+    let when = aleatorio(whens);  
+    
+    return `${who} ${action} ${what} ${when}`;  
+  }
+
+  document.getElementById("excuse").innerHTML = generarExcusa();
+  console.log (generarExcusa); 
 };
+
